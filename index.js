eab2ac8cef7e3c90e9628b7b4b81b5dcd2bfd957
unknown <minhajurrohoman9016@gmail.com>
2021-04-09 00:25:20 +0600

    adding

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const MongoClient = require('mongodb').MongoClient;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qxrx9.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 const app = express()
 app.use(bodyParser.json())
